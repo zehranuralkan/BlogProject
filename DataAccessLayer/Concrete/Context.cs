c0b567732bb3bea9c58899088c710851a74e74bd
ZEHRA <z.nuralkan@gmail.com>
2021-10-14 09:43:50 +0300

    ASP.NET
    14.10.2021

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+   public  class Context:DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder
+            )
+        {
+
+            optionsBuilder.UseSqlServer("server=LAPTOP-LAVUU4UR;database=CoreBlogDb; integrated security=true;");
+
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ EntityLayer/Concrete/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+   public class Category
+    {
+        //erisim belirleyici türü -değisken türü -isim {get set}
+        [Key]
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public string CategoryDescription { get; set; }
+
+        public bool CategoryStatus { get; set; }
+        //category'i aktif ya da pasife çekmek için
+
+
+    }
+}
