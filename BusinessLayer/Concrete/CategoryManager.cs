aaeba717e4977720aa20d4dc1127899cc1b85452
ZEHRA <z.nuralkan@gmail.com>
2021-10-17 16:09:26 +0300

    17.10.2021

--- BusinessLayer/Concrete/CategoryManager.cs
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -1,5 +1,5 @@
 ﻿using BusinessLayer.Abstract;
-using DataAccessLayer.Repositories;
+using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
@@ -11,36 +11,36 @@ namespace BusinessLayer.Concrete
 {
     public class CategoryManager : ICategoryService
     {
-            GenericRepository<Category> repo = new GenericRepository<Category>();
+        EFCategoryRepository eFCategoryRepository;
+        public CategoryManager()
+        {
+            eFCategoryRepository = new EFCategoryRepository();
+        }
             public void CategoryAdd(Category category)
         {
 
-            throw new NotImplementedException();
+            eFCategoryRepository.Insert(category);
         }
 
         public void CategoryDelete(Category category)
         {
-            if (category.CategoryID!= 0)
-            {
+            eFCategoryRepository.Delete(category);
 
-                repo.Delete(category);
-            }
-     
         }
 
         public void CategoryUpdate(Category category)
         {
-            throw new NotImplementedException();
+            eFCategoryRepository.Update(category);
         }
 
         public Category GetById(int id)
         {
-            throw new NotImplementedException();
+            return eFCategoryRepository.GetByID(id);
         }
 
         public List<Category> GetList()
         {
-            throw new NotImplementedException();
+            return eFCategoryRepository.GetListAll();
         }
     }
 }
