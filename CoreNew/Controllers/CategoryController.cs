4013e981572a3f17bb702c8ea850b340b707fb34
zehranuralkan <z.nuralkan@gmail.com>
2021-10-20 03:10:16 +0300

    22.ders

---
+++ CoreNew/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreNew.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoryManager cm = new CategoryManager(new EFCategoryRepository());
+        public IActionResult Index()
+        {
+            var values = cm.GetList();
+            return View(values);
+        }
+    }
+}
