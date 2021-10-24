bb267cd32e5dcfdb4a4d32d6e3bd24bc4d766358
zehranuralkan <z.nuralkan@gmail.com>
2021-10-24 22:06:59 +0300

    #28

--- CoreNew/Controllers/BlogController.cs
+++ CoreNew/Controllers/BlogController.cs
@@ -17,9 +17,10 @@ namespace CoreNew.Controllers
             return View(values);
         }
 
-        public IActionResult BlogDetails(int id)
+        public IActionResult BlogReadAll(int id)
         {
-            return View();
+            var values = bm.GetBlogByID(id);
+            return View(values);
         }
     }
 }
