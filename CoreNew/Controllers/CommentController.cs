4a98d7194fddaba4b5f55b3a94b327b3b4c49e77
zehranuralkan <z.nuralkan@gmail.com>
2021-11-05 18:17:51 +0300

    #30

--- CoreNew/Controllers/CommentController.cs
+++ CoreNew/Controllers/CommentController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,8 +8,10 @@ using System.Threading.Tasks;
 
 namespace CoreNew.Controllers
 {
-    public class CommentController : Controller
+     public class CommentController : Controller
     {
+        CommentManager cm = new CommentManager(new EFCommentRepository());
+
         public IActionResult Index()
         {
             return View();
@@ -16,9 +20,10 @@ namespace CoreNew.Controllers
         {
             return PartialView();
         }
-        public PartialViewResult CommentListByBlog()
+        public PartialViewResult CommentListByBlog(int id)
         {
-            return PartialView();
+           var values= cm.GetList(id);
+            return PartialView(values);
         }
     }
 }
---
+++ CoreNew/ViewComponents/CommentList.cs
@@ -0,0 +1,38 @@
+﻿using CoreNew.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreNew.ViewComponents
+{
+    public class CommentList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var commentvalues = new List<UserComment>
+            {
+                new UserComment
+                {
+                    ID=1,
+                    UserName="Zehra"
+                },
+                new UserComment
+                {
+                    ID=2,
+                    UserName="Bulut"
+                },
+                new UserComment
+                {
+                    ID=3,
+                    UserName="Büşra"
+                },
+
+            };
+            return View(commentvalues);
+        }
+
+
+    }
+}
