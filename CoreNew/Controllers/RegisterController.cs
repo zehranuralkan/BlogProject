d0bd7624af25851ca321b19c28bdc0e49b92350f
zehranuralkan <z.nuralkan@gmail.com>
2021-11-15 14:34:32 +0300

    #33

---
+++ CoreNew/Controllers/RegisterController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreNew.Controllers
+{
+    public class RegisterController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index()
+        {
+            return 
+        }
+    }
+}
