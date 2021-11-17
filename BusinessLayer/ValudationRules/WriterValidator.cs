4f689df95d167c34631f54269c8a855f4a26b0fa
zehranuralkan <z.nuralkan@gmail.com>
2021-11-18 01:21:23 +0300

    #36

---
+++ BusinessLayer/ValudationRules/WriterValidator.cs
@@ -0,0 +1,25 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValudationRules
+{
+    public class WriterValidator: AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar adı kısmı boş geçilemez.");
+            RuleFor(x => x.WriterMail).NotEmpty().WithMessage("Mail adresiboş geçilemez ");
+            RuleFor(x => x.WriterPassword).NotEmpty().WithMessage("Şifre boş geçilemez");
+            RuleFor(x => x.WriterName).MinimumLength(2).WithMessage("Lütfen en az 2 karakter girişi yapınız.");
+            RuleFor(x => x.WriterName).MaximumLength(50).WithMessage("Lütfen en fazla 50 karakterli veri girişi yapınız.");
+
+
+                
+                }
+    }
+}
--- CoreNew/Controllers/RegisterController.cs
+++ CoreNew/Controllers/RegisterController.cs
@@ -1,6 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValudationRules;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -21,10 +23,25 @@ namespace CoreNew.Controllers
         [HttpPost]
         public IActionResult Index(Writer p)
         {
-            p.WriterStatus = true;
-            p.WriterAbout = "Deneme Test";
-            wm.WriterAdd(p);
-            return RedirectToAction("Index", "Blog");
+            WriterValidator wv = new WriterValidator();
+            ValidationResult result = wv.Validate(p);
+            if(result.IsValid)
+            {
+                p.WriterStatus = true;
+                p.WriterAbout = "Deneme Test";
+                wm.WriterAdd(p);
+                return RedirectToAction("Index", "Blog");
+            }
+
+            else
+            {
+                foreach(var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+
+            }
+            return View();
         }
     }
 }
